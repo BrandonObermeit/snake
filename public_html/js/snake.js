0bacc8faa67064b73d4d9bc0bb1ae56b22ca23d9
Brandon Obermeit <BladeGX4@gmail.com>
2014-10-09 14:04:22 -0700

    ok

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -7,7 +7,10 @@ var screenWidth;
 var screenHeight;
 
 gameInitialize();
+snakeInitialize();
 gameDraw();
+snakeDraw();
+snakeUpdate();
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -34,7 +37,7 @@ function snakeInitialize (){
     snakeLength = 2;
     snakeSize = 20;
     
-    for(var index = 0; index < snakeLength; index++) {
+    for(var index = snakeLength - 1; index >= 0; index--) {
         snake.push( {
            x: index,
            y: 0
@@ -46,9 +49,20 @@ function snakeInitialize (){
 
 
 function snakeDraw () {
-    
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "blue";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
 }
 
 function snakeUpdate () {
+    var snakeHeadX =snake[0].x;
+    var snakeHeadY =snake[0].y;
     
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.x = snakeHeadX;
+    snake.unshift(snakeTail);
 }
